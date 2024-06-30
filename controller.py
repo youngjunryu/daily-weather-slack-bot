53749d84e686ec6990b434868877df85396d4f0c
ecvheo1 <ecvheo@naver.com>
2024-06-30 20:27:54 +0900

    add sending message to slack for test

--- controller.py
+++ controller.py
@@ -1,9 +1,22 @@
+import os
 import sys
+
+import requests
+from dotenv import load_dotenv
+
+
+def send_slack_message(slack_webhook_url):
+    requests.post(slack_webhook_url, json={"text": "Forecasting..."})
 
 
 def forecast():
-    # TODO: Implement forecast function
     print("Forecasting...")
+
+    load_dotenv()
+    service_key = os.environ.get("SERVICE_KEY")
+    slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+
+    send_slack_message(slack_webhook_url)
 
 
 def run():
