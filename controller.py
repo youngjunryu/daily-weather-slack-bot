9b8d8046f69095a0f6ffa8b809524b659fe46557
ecvheo1 <ecvheo@naver.com>
2024-06-30 19:35:44 +0900

    add initial controller with forecast command handling

---
+++ controller.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def forecast():
+    # TODO: Implement forecast function
+    print()
+
+
+def run():
+    if len(sys.argv) < 2:
+        print("Usage: python controller.py forecast")
+        return
+
+    if sys.argv[1] == "forecast":
+        forecast()
+
+
+if __name__ == "__main__":
+    run()
