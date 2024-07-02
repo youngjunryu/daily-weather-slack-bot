5a9937a29a9cb1833d70ad35bdef09f320e92431
ecvheo1 <ecvheo@naver.com>
2024-07-02 23:33:01 +0900

    implement request daily weather API

--- infrastructure/client/api_client.py
+++ infrastructure/client/api_client.py
@@ -1,7 +1,13 @@
 import requests
 
+from infrastructure.api.slack_api_request import SlackApiRequest
+from infrastructure.api.weather_api_request import WeatherApiRequest
+
 
 class ApiClient:
 
-    def send_slack_message(self, api_request):
+    def send_slack_message(self, api_request: SlackApiRequest):
         requests.post(api_request.url, json=api_request.body)
+
+    def request_daily_weather(self, api_request: WeatherApiRequest):
+        return requests.get(api_request.url, params=api_request.params)
